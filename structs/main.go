a53083ba4eb1549bbca26f34500320d9c226eb2c
Leandro Hespanhol <leandro.hespanhol@mercadolivre.com>
2022-09-26 08:42:19 -0300

    structss

--- structs/main.go
+++ structs/main.go
@@ -10,7 +10,7 @@ type contactInfo struct {
 type person struct {
 	firstName string
 	lastName string
-	contact contactInfo
+	contactInfo
 }
 
 func main() {
@@ -18,10 +18,31 @@ func main() {
 		alex := person{
 			firstName: "Alex",
 			lastName: "Anderson",
-			contact: contactInfo{
+			contactInfo: contactInfo{
 				email: "alex@gmail.com",
 				zip: 30310020,
 		}}
-		fmt.Println(alex)
-		// fmt.Printf("%+v", alex)
+		// alexPointer := &alex
+		alex.updateName("Alexa")
+		alex.print()
+
+		name  := "bill"
+		fmt.Println(&name) // The & turns a value into a pointer
+
+		garota := "Carrey"
+		updateName(garota)
+		fmt.Println(garota)
+}
+
+func (pointerToPerson *person) updateName(firstName string) { // *person indica que a func só pode ser chamada com um pointer para person
+	(*pointerToPerson).firstName = firstName // The * turns a pointer into a value
+}
+
+func updateName(n string) {
+	n = "Cloey"
+}
+
+func (p person) print() {
+	fmt.Printf("%+v", p)
+	// fmt.Println(p)
 }
